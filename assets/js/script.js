63c8a909a0bcd3c0bcc095a0240648277297fd57
kodapassey <kodapassey@charter.net>
2022-05-25 22:33:38 -0700

    fixed bug with empty tasks, cleared input field after task submission, added the ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,42 @@ var formEL = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
 
     event.preventDefault();
 
-    var listItemEl = document.createElement('li');
-    listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
-    tasksToDoEl.appendChild(listItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert('You need to fill out the task form!');
+        return false;
+    };
+
+    formEL.reset();
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
 };
 
-formEL.addEventListener('submit', createTaskHandler);
+var createTaskEl = function (taskDataObj) {
+    var listItemEl = document.createElement('li');
+    listItemEl.className = 'task-item';
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement('div');
+    // give it a class name
+    taskInfoEl.className = 'task-info';
+    // add HTML content to div
+    taskInfoEl.innerHTML = '<h3 class ="task-name">' + taskDataObj.name + '</h3><span class="task-type">' + taskDataObj.type + '</span>';
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+formEL.addEventListener('submit', taskFormHandler);
